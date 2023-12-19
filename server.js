434786fd00ac392b61381df3c2aa65a7d7d1b008
Simonas Tamkevicius <simonas.ta@icloud.com>
2023-12-19 11:32:28 -0500

    changed cors origin settings

--- server.js
+++ server.js
@@ -58,10 +58,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", corsOptions.origin.join(","));
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
