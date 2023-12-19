7523277e96069bfa3d1339b2ce5f4a7d99def8ac
Simonas Tamkevicius <simonas.ta@icloud.com>
2023-12-19 11:26:41 -0500

    changed cors origin settings

--- server.js
+++ server.js
@@ -66,8 +66,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: '*',
-  // origin: ["https://www.comillainc.com"],
+  origin: ["https://www.comillainc.com"],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
