bff0a9c597f0acfa36d7cc06e29d42dfe9fc04bf
Simonas Tamkevicius <simonas.ta@icloud.com>
2023-12-19 12:40:48 -0500

    changed position of response headers

--- server.js
+++ server.js
@@ -70,7 +70,15 @@ app.use(cors(corsOptions));
 
 // app.use("/uploads", express.static("uploads"));
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Specify your destination directory
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  },
+});
 
 const upload = multer({
   storage: storage,
